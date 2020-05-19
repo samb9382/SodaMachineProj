8e1429ecf9e167a7618aa19b25aedec8f6070e5e
SammySupreme B <samb9382@gmail.com>
2020-05-19 12:12:41 -0500

    created a list of coins and cans in the soda machine

--- Soda_Machine_Proj/SodaMachine.cs
+++ Soda_Machine_Proj/SodaMachine.cs
@@ -8,5 +8,41 @@ namespace Soda_Machine_Proj
 {
     class SodaMachine
     {
-    }
+        List<Coin> coins;
+
+        List<Can> cans;
+
+        public SodaMachine() //list of coins in the soda machine
+        {
+            cans = new List<Can>();
+        }
+
+        public void AddCan(Can can)
+        {
+            cans.Add(can);
+        }
+
+        public void RemoveCan(Can can)
+        {
+            cans.Remove(can);
+        }
+
+
+
+        public SodaMachine() //list of sodas in the soda machine
+        {
+            coins = new List<Coin>();
+        }
+
+        public void AddCoin(Coin coin)
+        {
+            coins.Add(coin);
+        }
+
+        public void RemoveCoin(Coin coin)
+        {
+            coins.Remove(coin);
+
+        }
+}
 }
