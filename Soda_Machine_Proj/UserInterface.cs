a188abcbd3b308227ca6f418e296f06892b8e295
SammySupreme B <samb9382@gmail.com>
2020-05-20 14:03:15 -0500

    created a choose soda option in the user interface giving the user the sodas to choose from

--- Soda_Machine_Proj/UserInterface.cs
+++ Soda_Machine_Proj/UserInterface.cs
@@ -8,6 +8,30 @@ namespace Soda_Machine_Proj
 {
     class UserInterface
     {
-        Console.WriteLine("Welcome");
+        public string sodaChoice;
+
+        public UserInterface()
+        {
+            Console.WriteLine("Welcome");
+
+        }
+
+        public void DisplayOptions()
+        {
+
+            Console.WriteLine("Please make a drink selection:");
+            Console.WriteLine("One For RootBeer");
+            Console.WriteLine("Two For OrangeSoda");
+            Console.WriteLine("Three For Cola");
+        }
+
+        public void ChooseSoda()
+        {
+            Console.WriteLine("Select a Soda: COLA, ROOTBEER, ORANGESODA");
+            sodaChoice = Console.ReadLine();
+            sodaChoice = sodaChoice.ToUpper();
+            Console.WriteLine("You chose " + sodaChoice);
+        }
+
     }
 }
